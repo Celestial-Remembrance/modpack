8758e01c6805be9915f4dae521fabaf6159f8cad
DevDyna <andrearisorti2003@gmail.com>
2024-03-09 16:42:08 +0100

    Create login.js

---
+++ kubejs/server_scripts/login.js
@@ -0,0 +1,16 @@
+PlayerEvents.loggedIn(event=>{
+    if (!event.player.stages.has('welcome')) {
+      event.player.stages.add('welcome')
+
+        event.player.give('minecraft:iron_pickaxe')
+        event.player.give('3x minecraft:apple')
+        event.player.tell("Welcome to the MOON")
+        event.player.setHeadArmorItem('ad_astra:space_helmet')
+        event.player.setChestArmorItem(Item.of('ad_astra:space_suit', '{BotariumData:{StoredFluids:[{Amount:10000L,Fluid:"ad_astra:oxygen"}]}}'))
+        event.player.setLegsArmorItem('ad_astra:space_pants')
+        event.player.setFeetArmorItem('ad_astra:space_boots')
+
+
+    }
+
+})
