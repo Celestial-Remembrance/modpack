eaa77edd36e29665666217ed3ba5334648220dcb
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 17:37:07 +0200

    Update .global.js

--- kubejs/client_scripts/.global.js
+++ kubejs/client_scripts/.global.js
@@ -1,58 +1,109 @@
 //priority : 1
-function convertString(input) {
-    const charMap = {
-      a: "ᴀ",
-      b: "ʙ",
-      c: "ᴄ",
-      d: "ᴅ",
-      e: "ᴇ",
-      f: "ꜰ",
-      g: "ɢ",
-      h: "ʜ",
-      i: "i",
-      j: "ᴊ",
-      k: "ᴋ",
-      l: "ʟ",
-      m: "ᴍ",
-      n: "ɴ",
-      o: "ᴏ",
-      p: "ᴘ",
-      q: "ꞯ",
-      r: "ʀ",
-      s: "ꜱ",
-      t: "ᴛ",
-      u: "ᴜ",
-      v: "ᴠ",
-      w: "ᴡ",
-      x: "x",
-      y: "ʏ",
-      z: "ᴢ",
-    };
-  
-    let result = "";
-    for (let char of input.toLowerCase()) {
-      result += charMap[char] || char;
-    }
-  
-    return result;
-  }
 
-  let verify = (what, type, x, y, builder) => {
-    if (what != "minecraft:air" && what != undefined && what != null) {
-      builder.addSlot(type, x, y).addItemStack(Item.of(what));
-    } else {
-      builder.addSlot(type, x, y).addItemStack(Item.of("minecraft:air"));
-    }
+/**
+ *
+ * @param {string} input
+ * @returns ᴀʙᴄᴅᴇꜰɢʜiᴊᴋʟᴍɴᴏᴘꞯʀꜱᴛᴜᴠᴡxʏᴢ
+ */
+function convertString(input) {
+  const charMap = {
+    a: "ᴀ",
+    b: "ʙ",
+    c: "ᴄ",
+    d: "ᴅ",
+    e: "ᴇ",
+    f: "ꜰ",
+    g: "ɢ",
+    h: "ʜ",
+    i: "i",
+    j: "ᴊ",
+    k: "ᴋ",
+    l: "ʟ",
+    m: "ᴍ",
+    n: "ɴ",
+    o: "ᴏ",
+    p: "ᴘ",
+    q: "ꞯ",
+    r: "ʀ",
+    s: "ꜱ",
+    t: "ᴛ",
+    u: "ᴜ",
+    v: "ᴠ",
+    w: "ᴡ",
+    x: "x",
+    y: "ʏ",
+    z: "ᴢ",
   };
 
-  let verifyCrude = (what, type, x, y, builder) => {
+  let result = "";
+  for (let char of input.toLowerCase()) {
+    result += charMap[char] || char;
+  }
+
+  return result;
+}
+
+/**
+ *
+ * @param {ItemStack} what
+ * @param {string} type Input|Output
+ * @param {int} x
+ * @param {int} y
+ * @param {Internal.IRecipeLayoutBuilder} builder
+ */
+let verify = (what, type, x, y, builder) => {
+  if (what != "minecraft:air" && what != undefined && what != null) {
+    builder.addSlot(type, x, y).addItemStack(Item.of(what));
+  } else {
+    builder.addSlot(type, x, y).addItemStack(Item.of("minecraft:air"));
+  }
+};
+
+/**
+ *
+ * @param {ItemStack} what
+ * @param {string} type Input|Output
+ * @param {int} x
+ * @param {int} y
+ * @param {Internal.IRecipeLayoutBuilder} builder
+ */
+let verifyCrude = (what, type, x, y, builder) => {
+  if (what != "minecraft:air" && what != undefined && what != null) {
+    builder.addSlot(type, x, y).addItemStack(what);
+  } else {
+    builder.addSlot(type, x, y).addItemStack(Item.of("minecraft:air"));
+  }
+};
+
+/**
+ *
+ * @param {FluidStack} what
+ * @param {string} type Input|Output
+ * @param {int} x
+ * @param {int} y
+ * @param {Internal.IRecipeLayoutBuilder} builder
+ */
+let verifyFluid = (what, type, x, y, builder) => {
+  builder.addSlot(type, x, y).addFluidStack(what, 1000);
+};
+
+/**
+ *
+ * @param {string} stack Item|Block|Fluid
+ * @param {*} what
+ * @param {string} type Input|Output
+ * @param {int} x
+ * @param {int} y
+ * @param {Internal.IRecipeLayoutBuilder} builder
+ */
+let verifyMixed = (stack, what, type, x, y, builder) => {
+  if (stack == "item" || stack == "block") {
     if (what != "minecraft:air" && what != undefined && what != null) {
       builder.addSlot(type, x, y).addItemStack(what);
     } else {
       builder.addSlot(type, x, y).addItemStack(Item.of("minecraft:air"));
     }
-  };
-
-  let verifyFluid = (what, type, x, y, builder) =>{
-    builder.addSlot(type, x, y).addFluidStack(what, 1000)
+  } else if (stack == "fluid") {
+    builder.addSlot(type, x, y).addFluidStack(what, 1000);
   }
+};
