6e81421a539dd22dd3ca997c4fc33d682d35221f
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 21:46:23 +0200

    Update jei-recipes.js

--- kubejs/startup_scripts/custom/jei-recipes.js
+++ kubejs/startup_scripts/custom/jei-recipes.js
@@ -1,22 +1,34 @@
 // priority: 1
 global.jei = {
   recipes: {
-    click: [{
-      input: {
-        main_hand: "minecraft:flint",
-        off_hand: "",
-        block: "minecraft:cobblestone",
-        extra: "",
+    click: [
+      {
+        input: {
+          main_hand: "minecraft:flint",
+          off_hand: "",
+          block: "minecraft:cobblestone",
+          extra: "",
+        },
+        output: {
+          type: "fluid",
+          block_replace: "minecraft:water",
+          drop: [],
+          chance: [],
+          isCrouching: false,
+          extra: "",
+        },
       },
-      output: {
-        type : "fluid",
-        block_replace: "minecraft:water",
-        drop: [],
-        chance: [],
-        isCrouching: false,
-        extra: "",
+    ],
+
+    atomic: [
+      {
+        input: "minecraft:diamond_ore",
+        output: "minecraft:diamond_block",
       },
-    }
+      {
+        input: "minecraft:coal_ore",
+        output: "minecraft:coal_block",
+      },
     ],
   },
 };
