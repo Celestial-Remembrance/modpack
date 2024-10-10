03ab02463bae3a502877e364bf0d41abb0d9a61f
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 17:38:01 +0200

    Create .global.js

---
+++ kubejs/server_scripts/.global.js
@@ -0,0 +1,22 @@
+// priority: 1
+function rnd(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rnd50() {
+  return Math.random() < 0.5;
+}
+
+function rnd75() {
+  return Math.random() < 0.75;
+}
+
+function rnd25() {
+  return Math.random() < 0.25;
+}
+
+function rndC(value) {
+  return Math.random() < (value == undefined ? 1 : value * 0.01);
+}
+
+
