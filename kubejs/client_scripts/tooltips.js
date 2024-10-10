6f5ffc835f53d06be75bf5e94d43b578ed206b5a
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 20:18:40 +0200

    Create tooltips.js

---
+++ kubejs/client_scripts/tooltips.js
@@ -0,0 +1,61 @@
+ItemEvents.tooltip((event) => {
+  function lazytip(ids, txts) {
+    if(!Array.isArray(ids)) ids = [ids]
+    ids.forEach((id, index) => {
+      event.addAdvanced(id, (item, advanced, text) => {
+        if (!event.isShift()) {
+          text.add(1, "§7Press §fShift§7 to see more info");
+        } else {
+            event.
+          txts.forEach((tip) => {
+            text.add(1, tip[index]);
+          });
+        }
+      });
+    });
+  }
+
+  lazytip(
+    [
+      "thoriumreactors:nickel_block",
+      "thoriumreactors:niob_block",
+      "thoriumreactors:molybdenum_block",
+    ],
+    [
+      [
+        "§8[§fMax Energy : §c114k ⚡/t§8]",
+        "§8[§fMax Energy : §e171k ⚡/t§8]",
+        "§8[§fMax Energy : §a228k ⚡/t§8]",
+      ],
+      [
+        "§8[§fModifier : §c1.0§8]",
+        "§8[§fModifier : §e1.5§8]",
+        "§8[§fModifier : §a2.0§8]",
+      ],
+    ]
+  );
+});
+/*
+§0 Black
+§1 Blue
+§2 Green
+§3 Cyan
+§4 Red
+§5 Magenta
+§6 Yellow
+§7 Gray
+§8 Dark Gray
+§9 Light Blue
+§a Light Green
+§b Light Cyan
+§c Light Red
+§d Light Magenta
+§e Light Yellow
+§f White
+.
+§l Bold
+§m Strikethrough
+§n Underline
+§o Italic
+§r Reset
+*/
