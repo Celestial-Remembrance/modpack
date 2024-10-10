f8275debda35b383d2149c48b25b2311ad7bea14
DevDyna <andrearisorti2003@gmail.com>
2024-10-11 00:36:38 +0200

    Update items&blocks.js

--- kubejs/startup_scripts/items&blocks.js
+++ kubejs/startup_scripts/items&blocks.js
@@ -1,4 +1,26 @@
 // priority: -1
-StartupEvents.registry('item',event=>{
-event.create('kubejs:star').texture("kubejs:item/star")
-})
+StartupEvents.registry("item", (event) => {
+  event.create("kubejs:star").texture("kubejs:item/star");
+
+  potion(event, "kubejs:limewater", 0xffffaf);
+
+  let types = [
+    {
+      id: "kubejs:tritium",
+      color: 0x8df2d4,
+    },
+    {
+      id: "kubejs:tritanium",
+      color: 0xcd57f0,
+    },
+  ];
+
+  types.forEach((e, index) => {
+    ingot(event, e.color, e.id);
+    dust(event, e.color, e.id);
+    dirty_dust(event, e.color, e.id, true);
+    nugget(event, e.color, e.id, true);
+    clump(event, e.color, e.id);
+    shard(event, e.color, e.id);
+  });
+});
