e5022b1c88d44f50e1b438d24bba50b815426bfd
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 17:37:32 +0200

    Create jei-recipes.js

---
+++ kubejs/startup_scripts/custom/jei-recipes.js
@@ -0,0 +1,22 @@
+// priority: 1
+global.jei = {
+  recipes: {
+    click: [{
+      input: {
+        main_hand: "minecraft:flint",
+        off_hand: "",
+        block: "minecraft:cobblestone",
+        extra: "",
+      },
+      output: {
+        type : "fluid",
+        block_replace: "minecraft:water",
+        drop: [],
+        chance: [],
+        isCrouching: false,
+        extra: "",
+      },
+    }
+    ],
+  },
+};
