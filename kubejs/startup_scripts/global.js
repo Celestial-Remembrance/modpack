cd2ff815040b567f344ca9c4c440eb861740a4ee
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 18:43:57 +0200

    Update global.js

--- kubejs/startup_scripts/global.js
+++ kubejs/startup_scripts/global.js
@@ -1,1 +1,26 @@
+// priority: 999
 Platform.mods.kubejs.name = "OxygenUtils";
+
+const $BooleanProperty = Java.loadClass(
+    "net.minecraft.world.level.block.state.properties.BooleanProperty"
+  );
+  const $IntegerProperty = Java.loadClass(
+    "net.minecraft.world.level.block.state.properties.IntegerProperty"
+  );
+  
+  function rnd(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  function rnd50() {
+    return Math.random() < 0.5;
+  }
+  
+  function rnd75() {
+    return Math.random() < 0.75;
+  }
+  
+  function rnd25() {
+    return Math.random() < 0.25;
+  }
+  
