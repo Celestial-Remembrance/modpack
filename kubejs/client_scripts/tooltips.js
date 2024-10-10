6d16326c3e010698db20841629d588f17c660838
DevDyna <andrearisorti2003@gmail.com>
2024-10-10 20:40:32 +0200

    Update tooltips.js

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -1,40 +1,40 @@
-ItemEvents.tooltip((event) => {
-  function lazytip(ids, txts) {
-    if(!Array.isArray(ids)) ids = [ids]
-    ids.forEach((id, index) => {
-      event.addAdvanced(id, (item, advanced, text) => {
-        if (!event.isShift()) {
-          text.add(1, "§7Press §fShift§7 to see more info");
-        } else {
-            event.
-          txts.forEach((tip) => {
-            text.add(1, tip[index]);
-          });
-        }
-      });
-    });
-  }
+// ItemEvents.tooltip((event) => {
+//   function lazytip(ids, txts) {
+//     if(!Array.isArray(ids)) ids = [ids]
+//     ids.forEach((id, index) => {
+//       event.addAdvanced(id, (item, advanced, text) => {
+//         if (!event.isShift()) {
+//           text.add(1, "§7Press §fShift§7 to see more info");
+//         } else {
+//             event.
+//           txts.forEach((tip) => {
+//             text.add(1, tip[index]);
+//           });
+//         }
+//       });
+//     });
+//   }
 
-  lazytip(
-    [
-      "thoriumreactors:nickel_block",
-      "thoriumreactors:niob_block",
-      "thoriumreactors:molybdenum_block",
-    ],
-    [
-      [
-        "§8[§fMax Energy : §c114k ⚡/t§8]",
-        "§8[§fMax Energy : §e171k ⚡/t§8]",
-        "§8[§fMax Energy : §a228k ⚡/t§8]",
-      ],
-      [
-        "§8[§fModifier : §c1.0§8]",
-        "§8[§fModifier : §e1.5§8]",
-        "§8[§fModifier : §a2.0§8]",
-      ],
-    ]
-  );
-});
+//   lazytip(
+//     [
+//       "thoriumreactors:nickel_block",
+//       "thoriumreactors:niob_block",
+//       "thoriumreactors:molybdenum_block",
+//     ],
+//     [
+//       [
+//         "§8[§fMax Energy : §c114k ⚡/t§8]",
+//         "§8[§fMax Energy : §e171k ⚡/t§8]",
+//         "§8[§fMax Energy : §a228k ⚡/t§8]",
+//       ],
+//       [
+//         "§8[§fModifier : §c1.0§8]",
+//         "§8[§fModifier : §e1.5§8]",
+//         "§8[§fModifier : §a2.0§8]",
+//       ],
+//     ]
+//   );
+// });
 /*
 §0 Black
 §1 Blue
