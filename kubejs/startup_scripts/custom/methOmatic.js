cf2c9736f145be64d4b44638a02b40a21870f41f
DevDyna <andrearisorti2003@gmail.com>
2024-10-11 14:34:42 +0200

    Update methOmatic.js

--- kubejs/startup_scripts/custom/methOmatic.js
+++ kubejs/startup_scripts/custom/methOmatic.js
@@ -4,22 +4,11 @@ StartupEvents.registry("block", (event) => {
     event
       .create(element.block, "cardinal")
       .defaultCutout()
-      .textureAll(element.texture)
       .box(1, 0, 1, 14, 3, 14)
       .item((item) => {
         item.modelJson({
           parent: "kubejs:block/sample/mercury",
         });
-      }).blockstateJson = () => {
-      let planets = ["moon", "mars", "mercury", "venus", "glacio"];
-      let rot = [0, 90, 180, 270];
-      let list = [];
-      rot.forEach((r) => {
-        planets.forEach((e) => {
-          list.push({ model: "kubejs:block/sample/" + e, y: r });
-        });
-      });
-      return { variants: { "": list } };
-    };
+      }).blockstateJson = meteors();
   });
 });
