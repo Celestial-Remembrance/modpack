84000fe7761c97fe4f6ca11cca9b1b90ef029597
DevDyna <andrearisorti2003@gmail.com>
2024-10-12 14:57:40 +0200

    Update items&blocks.js

--- kubejs/startup_scripts/items&blocks.js
+++ kubejs/startup_scripts/items&blocks.js
@@ -26,6 +26,9 @@ let types = [
   },
 ];
 
+let Cblock = '_crystal_block'
+let Cshard = '_shard'
+
 StartupEvents.registry("item", (event) => {
   event.create("kubejs:star").texture("kubejs:item/star");
   event.create("kubejs:star2").texture("kubejs:item/star-2");
@@ -40,5 +43,19 @@ StartupEvents.registry("item", (event) => {
 StartupEvents.registry("block", (event) => {
   types.forEach((e, index) => {
     crystal_block(event, e.id, e.color);
+  });
+
+  global.resources.meteorites.forEach((element) => {
+    event
+      .create(element.block)
+      .defaultCutout()
+      .soundType(SoundType.ANCIENT_DEBRIS)
+      .box(1, 0, 1, 14, 3, 14)
+      .noDrops()
+      .item((item) => {
+        item.modelJson({
+          parent: "kubejs:block/sample/"+ element.icon,
+        });
+      }).blockstateJson = meteors(element.type);
   });
 });
