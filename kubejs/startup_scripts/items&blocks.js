63203fcaa6d6af11bec38d1c4160d30f95300c21
DevDyna <andrearisorti2003@gmail.com>
2024-10-12 15:05:39 +0200

    Update items&blocks.js

--- kubejs/startup_scripts/items&blocks.js
+++ kubejs/startup_scripts/items&blocks.js
@@ -1,33 +1,4 @@
 // priority: -1
-let types = [
-  {
-    id: "kubejs:copper",
-    color: 0xffb451,
-  },
-  {
-    id: "kubejs:iron",
-    color: 0xdbdbdb,
-  },
-  {
-    id: "kubejs:tin",
-    color: 0xa9e6dc,
-  },
-  {
-    id: "kubejs:redstone",
-    color: 0xf73737,
-  },
-  {
-    id: "kubejs:coal",
-    color: 0x616161,
-  },
-  {
-    id: "kubejs:nickel",
-    color: 0xf0dd8e,
-  },
-];
-
-let Cblock = '_crystal_block'
-let Cshard = '_shard'
 
 StartupEvents.registry("item", (event) => {
   event.create("kubejs:star").texture("kubejs:item/star");
@@ -35,14 +6,14 @@ StartupEvents.registry("item", (event) => {
 
   potion(event, "kubejs:limewater", 0xffffaf);
 
-  types.forEach((e, index) => {
-    shard(event, e.id, e.color, true);
+  global.resources.crystals.forEach((e, index) => {
+    shard(event, 'kubejs:'+e.id, e.color, true);
   });
 });
 
 StartupEvents.registry("block", (event) => {
-  types.forEach((e, index) => {
-    crystal_block(event, e.id, e.color);
+  global.resources.crystals.forEach((e, index) => {
+    crystal_block(event, 'kubejs:'+e.id, e.color);
   });
 
   global.resources.meteorites.forEach((element) => {
