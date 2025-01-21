9e0714fe2235bcf783f5007056cab558db1757ec
DevDyna <andrearisorti2003@gmail.com>
2025-01-21 13:35:40 +0100

    Update items&blocks.js

--- kubejs/startup_scripts/items&blocks.js
+++ kubejs/startup_scripts/items&blocks.js
@@ -5,35 +5,47 @@ StartupEvents.registry("item", (event) => {
   //event.create("kubejs:motor").texture("kubejs:item/motor");
   event.create("kubejs:star2").texture("kubejs:item/star-2");
 
-  event.create('icon_click').texture('kubejs:item/right_click')
-  event.create('icon_tree').texture('kubejs:item/tree')
+  event.create("icon_click").texture("kubejs:item/right_click");
+  event.create("icon_tree").texture("kubejs:item/tree");
 
-
-  potion(event, "kubejs:limewater", 0xffffaf);
+  // potion(event, "kubejs:limewater", 0xffffaf);
 
   global.resources.crystals.forEach((e) => {
-    shard(event, 'kubejs:'+e.id, e.color, true);
+    shard(event, "kubejs:" + e.id, e.color, true);
   });
 
+
+  event.create('vulcanic_alloy').texture("kubejs:item/alloy")
 
 });
 
 StartupEvents.registry("block", (event) => {
   global.resources.crystals.forEach((e) => {
-    crystal_block(event, 'kubejs:'+e.id, e.color);
+    crystal_block(event, "kubejs:" + e.id, e.color);
   });
 
   global.resources.gems.forEach((e) => {
-    raw_block(event, 'kubejs:'+e.id, e.color);
+    raw_block(event, "kubejs:" + e.id, e.color);
   });
 
   global.resources.meteorites.forEach((element) => {
-    meteors(event,element.block,element.type)
+    meteors(event, element.block, element.type);
   });
 
+  ore(event, "kubejs:fancy_cake", "gold", "ad_astra:block/moon_stone");
+});
 
-  ore(event,'kubejs:fancy_cake','gold','ad_astra:block/moon_stone')
-
-
-
+StartupEvents.registry("fluid", (event) => {
+  event
+    .create("magma")
+    .luminosity(15)
+    .temperature(1300)
+    .flowingTexture("minecraft:block/magma")
+    .stillTexture("minecraft:block/magma");
+  event
+    .create("liquid_etrium")
+    .luminosity(15)
+    .temperature(1300)
+    .thickTexture(0x7bfcd7)
+    .bucketColor(0x7bfcd76);
 });
