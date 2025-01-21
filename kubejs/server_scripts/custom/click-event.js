d8c867816e1784cf53dc01d4cad7c8efa2b6a7a9
DevDyna <andrearisorti2003@gmail.com>
2025-01-21 13:14:15 +0100

    Delete click-event.js

--- kubejs/server_scripts/custom/click-event.js
+++
@@ -1,7 +0,0 @@
-BlockEvents.broken("kubejs:dynamo", (event) => {
-  if (
-    !event.player.isCreative() &&
-    event.block.properties.get("active").toLowerCase() === "true"
-  )
-    event.block.popItem("kubejs:star");
-});
