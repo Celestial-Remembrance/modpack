7d2174142e539e77420c15c98a2cfc257b8c3174
DevDyna <andrearisorti2003@gmail.com>
2025-01-21 16:43:52 +0100

    Update tag.js

--- kubejs/server_scripts/tag.js
+++ kubejs/server_scripts/tag.js
@@ -1,3 +1,25 @@
-ServerEvents.tags("item",event=>{
-    event.add("kubejs:moderators",['thoriumreactors:molybdenum_block', 'thoriumreactors:niob_block', 'thoriumreactors:nickel_block'])
-})
+// priority: -1
+ServerEvents.tags("item", (event) => {
+  event.add(NAMEMOD + "moderators", [
+    "thoriumreactors:molybdenum_block",
+    "thoriumreactors:niob_block",
+    "thoriumreactors:nickel_block",
+  ]);
+  event.add(NAMEMOD + "alloy_ingot", [
+    "minecraft:iron_ingot",
+    "minecraft:copper_ingot",
+  ]);
+
+  //extractinator item tag decompiler
+  extractinator_recipe.tags.forEach((e, i) => {
+    event.add(NAMEMOD + e, extractinator_recipe.entry[i]);
+    event.add(NAMEMOD + e + TAG_RESULT, extractinator_recipe.result[i]);
+  });
+});
+
+ServerEvents.tags("block", (event) => {
+  //extractinator block tag decompiler
+  extractinator_recipe.tags.forEach((e, i) => {
+    event.add(NAMEMOD + e, extractinator_recipe.entry[i]);
+  });
+});
